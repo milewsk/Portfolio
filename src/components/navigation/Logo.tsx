62ddc5abd02bde9a1d8b2c95f0a7a9726863eddc
milewsk <milewski.konrad00@gmail.com>
2025-06-23 23:56:32 +0200

    changes in logo.tsx

---
+++ src/components/navigation/Logo.tsx
@@ -0,0 +1,12 @@
+
+const Logo = () => {
+    return (
+        <img
+            src="./src/assets/logo.png"
+            alt="Logo"
+            className="h-10 md:h-12 object-cover text-white"
+        />
+    );
+}
+
+export default Logo;
